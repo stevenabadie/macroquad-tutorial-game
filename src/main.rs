cc0ab83ae5d9cce85a874dfcbb4db0a195708925
Steven Abadie <steven@anothernewthing.com>
2025-03-31 21:56:14 -0600

    feat: add simple circle and controls

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,28 @@ use macroquad::prelude::*;
 
 #[macroquad::main("My game")]
 async fn main() {
+    let mut x = screen_width() / 2.0;
+    let mut y = screen_height() / 2.0;
+    let speed = 3.0;
+
     loop {
         clear_background(YELLOW);
+
+        if is_key_down(KeyCode::Right) {
+            x += speed;
+        }
+        if is_key_down(KeyCode::Left) {
+            x -= speed;
+        }
+        if is_key_down(KeyCode::Down) {
+            y += speed;
+        }
+        if is_key_down(KeyCode::Up) {
+            y -= speed;
+        }
+
+        draw_circle(x, y, 16.0, BLACK);
+
         next_frame().await
     }
 }

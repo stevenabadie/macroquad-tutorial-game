ca531549dc080a05395b904d38d5d9005be59c36
Steven Abadie <steven@anothernewthing.com>
2025-04-19 17:47:57 -0600

    chore: comment out shader to see if wasm works

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,22 @@ use macroquad::{prelude::*, rand::ChooseRandom};
 use macroquad_particles::{self as particles, ColorCurve, EmissionShape, Emitter, EmitterConfig};
 use std::fs;
 
-const FRAGMENT_SHADER: &str = include_str!("starfield-shader.glsl");
-const VERTEX_SHADER: &str = "#version 100
-attribute vec3 position;
-attribute vec2 texcoord;
-attribute vec4 color0;
-varying float iTime;
+// const FRAGMENT_SHADER: &str = include_str!("starfield-shader.glsl");
+// const VERTEX_SHADER: &str = "#version 100
+// attribute vec3 position;
+// attribute vec2 texcoord;
+// attribute vec4 color0;
+// varying float iTime;
 
-uniform mat4 Model;
-uniform mat4 Projection;
-uniform vec4 _Time;
+// uniform mat4 Model;
+// uniform mat4 Projection;
+// uniform vec4 _Time;
 
-void main() {
-    gl_Position = Projection * Model * vec4(position, 1);
-    iTime = _Time.x;
-}
-";
+// void main() {
+//     gl_Position = Projection * Model * vec4(position, 1);
+//     iTime = _Time.x;
+// }
+// ";
 
 struct Shape {
     size: f32,
@@ -155,41 +155,41 @@ async fn main() {
 
     let mut game_state = GameState::MainMenu;
 
-    let mut direction_modifier: f32 = 0.0;
-    let render_target = render_target(320, 150);
-    render_target.texture.set_filter(FilterMode::Nearest);
-    let material = load_material(
-        ShaderSource::Glsl {
-            vertex: VERTEX_SHADER,
-            fragment: FRAGMENT_SHADER,
-        },
-        MaterialParams {
-            uniforms: vec![
-                UniformDesc::new("iResolution", UniformType::Float2),
-                UniformDesc::new("direction_modifier", UniformType::Float1),
-            ],
-            ..Default::default()
-        },
-    )
-    .expect("Shader failed to compile");
+    // let mut direction_modifier: f32 = 0.0;
+    // let render_target = render_target(320, 150);
+    // render_target.texture.set_filter(FilterMode::Nearest);
+    // let material = load_material(
+    //     ShaderSource::Glsl {
+    //         vertex: VERTEX_SHADER,
+    //         fragment: FRAGMENT_SHADER,
+    //     },
+    //     MaterialParams {
+    //         uniforms: vec![
+    //             UniformDesc::new("iResolution", UniformType::Float2),
+    //             UniformDesc::new("direction_modifier", UniformType::Float1),
+    //         ],
+    //         ..Default::default()
+    //     },
+    // )
+    // .expect("Shader failed to compile");
 
     loop {
         clear_background(BLACK);
 
-        material.set_uniform("iResolution", (screen_width(), screen_height()));
-        material.set_uniform("direction_modifier", direction_modifier);
-        gl_use_material(&material);
-        draw_texture_ex(
-            &render_target.texture,
-            0.,
-            0.,
-            WHITE,
-            DrawTextureParams {
-                dest_size: Some(vec2(screen_width(), screen_height())),
-                ..Default::default()
-            },
-        );
-        gl_use_default_material();
+        // material.set_uniform("iResolution", (screen_width(), screen_height()));
+        // material.set_uniform("direction_modifier", direction_modifier);
+        // gl_use_material(&material);
+        // draw_texture_ex(
+        //     &render_target.texture,
+        //     0.,
+        //     0.,
+        //     WHITE,
+        //     DrawTextureParams {
+        //         dest_size: Some(vec2(screen_width(), screen_height())),
+        //         ..Default::default()
+        //     },
+        // );
+        // gl_use_default_material();
 
         match game_state {
             GameState::MainMenu => {
@@ -235,11 +235,11 @@ async fn main() {
 
                 if is_key_down(KeyCode::Right) {
                     circle.x += movement;
-                    direction_modifier += 0.05 * delta_time;
+                    // direction_modifier += 0.05 * delta_time;
                 }
                 if is_key_down(KeyCode::Left) {
                     circle.x -= movement;
-                    direction_modifier -= 0.05 * delta_time;
+                    // direction_modifier -= 0.05 * delta_time;
                 }
                 if is_key_down(KeyCode::Down) {
                     circle.y += movement;

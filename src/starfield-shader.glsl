70c745aa58a19ec5b44799187f94a1dd0eb0be42
Steven Abadie <steven@anothernewthing.com>
2025-04-19 10:41:25 -0600

    fix: trying to fix shader to see if that is the wasm issue

--- src/starfield-shader.glsl
+++ src/starfield-shader.glsl
@@ -1,5 +1,3 @@
-#version 100
-
 // Starfield Tutorial by Martijn Steinrucken aka BigWings - 2020
 // countfrolic@gmail.com
 // License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
@@ -7,10 +5,7 @@
 
 precision highp float;
 
-varying vec4 color;
-varying vec2 uv;
-varying float iTime;
-
+uniform float iTime;
 uniform vec2 iResolution;
 uniform float direction_modifier;
 
@@ -43,43 +38,42 @@ float Hash21(vec2 p) {
 }
 
 vec3 StarLayer(vec2 uv) {
-    vec3 col = vec3(0);
+    vec3 col = vec3(0.0);
 
-    vec2 gv = fract(uv) - .5;
+    vec2 gv = fract(uv) - 0.5;
     vec2 id = floor(uv);
 
     float t = iTime * 0.1;
     for (int y = -1; y <= 1; y++) {
         for (int x = -1; x <= 1; x++) {
-            vec2 offs = vec2(x, y);
+            vec2 offs = vec2(float(x), float(y));
 
-            float n = Hash21(id + offs); // random between 0 and 1
+            float n = Hash21(id + offs);
             float size = fract(n * 345.32);
-            float star = Star(gv - offs - vec2(n, fract(n * 42.)) + .5, smoothstep(.9, 1., size) * .6);
-            vec3 color = sin(vec3(.8, .8, .8) * fract(n * 2345.2) * 123.2) * .5 + .5;
-            color = color * vec3(0.25, 0.25, 0.20);
-            star *= sin(iTime * 3. + n * 6.2831) * .5 + 1.;
+            float star = Star(gv - offs - vec2(n, fract(n * 42.0)) + 0.5, smoothstep(0.9, 1.0, size) * 0.6);
+            vec3 color = sin(vec3(0.8) * fract(n * 2345.2) * 123.2) * 0.5 + 0.5;
+            color *= vec3(0.25, 0.25, 0.20);
+            star *= sin(iTime * 3.0 + n * 6.2831) * 0.5 + 1.0;
             col += star * size * color;
         }
     }
     return col;
 }
 
-void main()
-{
-    vec2 uv = (gl_FragCoord.xy - .5 * iResolution.xy) / iResolution.y;
-    float t = iTime * .02;
+void main() {
+    vec2 uv = (gl_FragCoord.xy - 0.5 * iResolution.xy) / iResolution.y;
+    float t = iTime * 0.02;
 
     float speed = 3.0;
     vec2 direction = vec2(-0.25 + direction_modifier, -1.0) * speed;
 
     uv += direction;
-    vec3 col = vec3(0);
+    vec3 col = vec3(0.0);
 
-    for (float i = 0.; i < 1.; i += 1. / NUM_LAYERS) {
-        float depth = fract(i+t);
-        float scale = mix(20., .5, depth);
-        float fade = depth * smoothstep(1., .9, depth);
+    for (float i = 0.0; i < 1.0; i += 1.0 / NUM_LAYERS) {
+        float depth = fract(i + t);
+        float scale = mix(20.0, 0.5, depth);
+        float fade = depth * smoothstep(1.0, 0.9, depth);
         col += StarLayer(uv * scale + i * 453.2) * fade;
     }
 
